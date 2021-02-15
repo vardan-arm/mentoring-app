7513eaf465a802156c1884b0acad7a09e11767ac
VardanHakobyan <vardan_live@live.com>
2021-02-15 22:58:16 +0400

    initial server setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app =express();
+
+app.get('/users', (req, res) => {
+  res.send(['user 1', 'user 2']);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
