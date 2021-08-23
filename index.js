8d92e855afdd0a12be9f2c9f76d002e55accf694
Vardan Hakobyan <vardanhakobyan@Vardans-MacBook-Pro.local>
2021-08-24 00:07:41 +0400

    chore: test commit

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const localeService = new LocaleService(i18n);
 console.log(localeService.getLocales()); // ['en', 'el']
 
 console.log('English: ', localeService.translate('login'));
-
 localeService.setLocale('am');
 console.log(localeService.getCurrentLocale()); // 'en'
 console.log('Armenian: ', localeService.translate('login'));
